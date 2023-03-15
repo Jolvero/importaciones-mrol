5e0934e170364f1070bd9cddac414e9a2e01397f
Jolvero <93722547+Jolvero@users.noreply.github.com>
2023-03-15 16:59:54 -0600

    ""

--- resources/views/email/despacho.blade.php
+++ resources/views/email/despacho.blade.php
@@ -9,9 +9,9 @@
 <body>
     @foreach ($despachos as $despacho)
     <h2>Estatus de despacho {{$despacho->referencia}}:<span>{{$despacho->despachos->nombre}}</span></h2>
-    <p >Consulta toda la información en <a href="http://importaciones.mrollogistics.com/importacion/{{$despacho->id}}">http://importaciones.mrollogistics.com/importacion/{{$despacho->id}}</a></p>
+    <p >Consulta toda la información en <a href="http://127.0.0.1:8000/importacion/{{$despacho->id}}">http://127.0.0.1:8000/importacion/{{$despacho->id}}</a></p>
 
-    <img style="display:block" src='http://importaciones.mrollogistics.com/images/logo.jpeg' alt="img-logo">
+    <img style="display:block" src='http://127.0.0.1:8000/images/logo.jpeg' alt="">
     @endforeach
 </body>
 </html>
